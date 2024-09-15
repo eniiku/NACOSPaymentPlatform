7b8711727b2ef94764ad1de76e9e12160afd1294
David Enikuomehin <enikuauth@gmail.com>
2024-09-15 23:57:30 +0100

    chore: overwrite spring-security with basic config

---
+++ src/main/java/com/nacosfunaabpay/paymentplatform/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.nacosfunaabpay.paymentplatform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()).httpBasic(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
---
+++ src/main/java/com/nacosfunaabpay/paymentplatform/model/Student.java
@@ -0,0 +1,4 @@
+package com.nacosfunaabpay.paymentplatform.model;
+
+public class Student {
+}
