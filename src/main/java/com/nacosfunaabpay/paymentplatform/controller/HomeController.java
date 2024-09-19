c02011c8666801e88aed57e23bd035ceba0ae49e
David Enikuomehin <enikuauth@gmail.com>
2024-09-19 23:58:42 +0100

    fix: fix failing homepage render

--- src/main/java/com/nacosfunaabpay/paymentplatform/controller/HomeController.java
+++ src/main/java/com/nacosfunaabpay/paymentplatform/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String hello() {
-        return "hello";
+    public String home() {
+        return "home";
     }
 
 }
