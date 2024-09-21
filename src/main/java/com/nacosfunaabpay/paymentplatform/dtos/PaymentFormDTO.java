2448ba3f0eca69f048a18d3f9078b66b2539c004
David Enikuomehin <enikuauth@gmail.com>
2024-09-21 23:56:37 +0100

    feat: create payment form data object

---
+++ src/main/java/com/nacosfunaabpay/paymentplatform/dtos/PaymentFormDTO.java
@@ -0,0 +1,68 @@
+package com.nacosfunaabpay.paymentplatform.dtos;
+
+public class PaymentFormDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String registrationNumber;
+    private String program;
+    private String level;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
