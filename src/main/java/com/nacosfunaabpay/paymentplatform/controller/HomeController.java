6973eb7e43ee91e8ad04ae18943e0dfae8c59d8a
David Enikuomehin <enikuauth@gmail.com>
2024-09-21 23:54:29 +0100

    feat: update homepage & controller

--- src/main/java/com/nacosfunaabpay/paymentplatform/controller/HomeController.java
+++ src/main/java/com/nacosfunaabpay/paymentplatform/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.nacosfunaabpay.paymentplatform.controller;
 
+import com.nacosfunaabpay.paymentplatform.dtos.PaymentFormDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
+    @GetMapping("")
+    public String renderHomePage(Model model) {
+        model.addAttribute("paymentForm", new PaymentFormDTO());
         return "home";
     }
 
