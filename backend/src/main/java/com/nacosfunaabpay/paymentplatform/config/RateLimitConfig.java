ed9fa776b4aba3a50a76a70ad5ddd3b5498277e0
David Enikuomehin <enikuauth@gmail.com>
2024-10-11 19:18:28 +0100

    fix: quick hotfixes

--- backend/src/main/java/com/nacosfunaabpay/paymentplatform/config/RateLimitConfig.java
+++ backend/src/main/java/com/nacosfunaabpay/paymentplatform/config/RateLimitConfig.java
@@ -1,6 +1,5 @@
 package com.nacosfunaabpay.paymentplatform.config;
 
-import com.giffing.bucket4j.spring.boot.starter.context.properties.Bucket4JConfiguration;
 import io.github.bucket4j.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +10,14 @@ import java.time.Duration;
 public class RateLimitConfig {
 
     @Bean
-    public Bucket4JConfiguration bucket4jConfiguration() {
+//  TODO: rework file to use bucket4jConfig
+    public Bucket bucket4jConfiguration() {
 
         Refill refill = Refill.intervally(10, Duration.ofMinutes(1));
         Bandwidth limit = Bandwidth.classic(10, refill);
 
-        return ((Bucket4JConfiguration) Bucket.builder()
+        return Bucket.builder()
                 .addLimit(limit)
-                .build());
+                .build();
     }
 }
