6a1b16efa58b9429b102b46f4260dc1f461f3338
David Enikuomehin <enikuauth@gmail.com>
2024-12-14 22:49:28 +0100

    fix: fix broken seed script

--- backend/src/main/resources/db/migration/V2__Insert_initial_data.sql
+++ backend/src/main/resources/db/migration/V2__Insert_initial_data.sql
@@ -6,12 +6,12 @@ VALUES ('CSC', 'Computer Science', 'Description for Computer Science'),
        ('ICT', 'Information and Communications Technology', 'Description for Information and Communications Technology'),
        ('SWE', 'Software Engineering', 'Study of software design and development'),
        ('CYS', 'Cybersecurity', 'Study of protecting systems, networks, and programs from digital attacks')
-    ON CONFLICT (name) DO NOTHING;
+    ON CONFLICT (key) DO NOTHING;
 
 INSERT INTO level (key, name, dues_amount)
 VALUES ('L100', '100 Level', 5000.00),
        ('L200F', '200 Level - Fresher(DE)', 5000.00),
        ('L200', '200 Level', 3500.00),
        ('L300', '300 Level', 3500.00),
-       ('L400', '400 Level', 3500.00);
-ON CONFLICT (name) DO NOTHING;
+       ('L400', '400 Level', 3500.00)
+    ON CONFLICT (key) DO NOTHING;
