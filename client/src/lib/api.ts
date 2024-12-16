0192f900865dfbcb84aedcfb9b79d64f6eafd0ee
David Enikuomehin <enikuauth@gmail.com>
2024-12-17 00:13:13 +0100

    Revert "chore(frontend): hardcode backend image as base backend url"
    This reverts commit 5efd8065e72bcc3ae1e873bd5d4b55c1bebfbf9e.

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const apiClient = axios.create({
     // baseURL: import.meta.env.BACKEND_API_BASE_URL || "http://localhost:8443/api/v1",
-    baseURL: "http://backend:8443/api/v1",
+    baseURL: "http://3.8.182.85:8443/api/v1",
     timeout: 30000,
     withCredentials: true,
 })
