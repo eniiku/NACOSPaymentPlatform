ef912550ee22a96be396d8da757eb54ef38ce0c6
David Enikuomehin <enikuauth@gmail.com>
2024-12-26 11:47:24 +0100

    feat(backend): reference allowed cors origin by env variables

--- backend/src/main/java/com/nacosfunaabpay/paymentplatform/config/SecurityConfig.java
+++ backend/src/main/java/com/nacosfunaabpay/paymentplatform/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.nacosfunaabpay.paymentplatform.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,6 +18,9 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${ALLOWED_CORS_ORIGINS:http://localhost:5173}")  // default for local development
+    private String allowedCorsOrigins;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,9 +43,10 @@ public class SecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+        List<String> allowedOrigins = List.of(allowedCorsOrigins.split(","));
+
         CorsConfiguration configuration = new CorsConfiguration();
-//        TODO:! I probably shouldn't be hardcoidng whitelisted production IP's
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://3.8.182.85:5173","http://3.8.182.85"));
+        configuration.setAllowedOrigins(allowedOrigins);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
