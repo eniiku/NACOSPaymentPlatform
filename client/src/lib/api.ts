aa19e510572e5d333eed5febf9e6b52b612f6736
David Enikuomehin <enikuauth@gmail.com>
2024-12-28 21:12:55 +0100

    feat(frontend): create proxy for api requests

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,9 +1,10 @@
 import axios from "axios"
 
-const backendApiBaseUrl = import.meta.env.VITE_BACKEND_API_BASE_URL
-
 export const apiClient = axios.create({
-    baseURL: backendApiBaseUrl ? `${backendApiBaseUrl}/api/v1` : "http://localhost:8443/api/v1",
+    baseURL: "/api/v1",
     timeout: 30000,
     withCredentials: true,
+    headers: {
+        "Content-Type": "application/json",
+    },
 })
