249d12926a42ac157344a0e41262d104cac371e8
David Enikuomehin <enikuauth@gmail.com>
2024-12-29 16:59:32 +0100

    feat(backend): update sendEmail logic to attach pdfs to emails

--- backend/src/main/java/com/nacosfunaabpay/paymentplatform/service/EmailServiceImpl.java
+++ backend/src/main/java/com/nacosfunaabpay/paymentplatform/service/EmailServiceImpl.java
@@ -1,25 +1,34 @@
 package com.nacosfunaabpay.paymentplatform.service;
 
+import com.lowagie.text.DocumentException;
 import com.nacosfunaabpay.paymentplatform.model.Invoice;
 import com.nacosfunaabpay.paymentplatform.model.Receipt;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.io.IOException;
 
 
 @Service
-public class EmailServiceImpl implements EmailService{
+public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
     @Autowired
     private TemplateEngine templateEngine;
+
+    private final PdfGenerationService pdfGenerationService;
+
+    public EmailServiceImpl(PdfGenerationService pdfGenerationService) {
+        this.pdfGenerationService = pdfGenerationService;
+    }
 
     @Override
     public void sendInvoiceEmail(Invoice invoice) {
@@ -28,41 +37,52 @@ public class EmailServiceImpl implements EmailService{
         context.setVariable("student", invoice.getStudent());
 
         String to = invoice.getStudent().getEmail();
-        String subject = "Your Invoice for NACOS Payment";
+        String subject = "Here's Your Invoice for NACOS Dues Payment";
         String templateName = "email/invoice";
+        String pdfFileName = String.format("%s_invoice", invoice.getStudent().getRegistrationNumber());
 
-        sendEmail(to, subject, templateName, context);
+        sendEmail(to, subject, templateName, context, "invoice", pdfFileName);
     }
 
     @Override
     public void sendReceiptEmail(Receipt receipt) {
         Context context = new Context();
         context.setVariable("receipt", receipt);
-        context.setVariable("payment", receipt.getPayment());
-        context.setVariable("invoice", receipt.getPayment().getInvoice());
-        context.setVariable("student", receipt.getPayment().getStudent());
 
         String to = receipt.getPayment().getStudent().getEmail();
-        String subject = "Your Receipt for NACOS Payment";
+        String subject = "Here's Your Receipt for NACOS Payment";
         String templateName = "email/receipt";
+        String pdfFileName = String.format("%s_receipt", receipt.getPayment().getStudent().getRegistrationNumber());
 
-        sendEmail(to, subject, templateName, context);
+        sendEmail(to, subject, templateName, context, "receipt", pdfFileName);
     }
 
-    private void sendEmail(String to, String subject, String templateName, Context context) {
+    private void sendEmail(String to,
+                           String subject,
+                           String emailTemplateName,
+                           Context context,
+                           String pdfTemplateName,
+                           String pdfFileName) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-            String htmlContent = templateEngine.process(templateName, context);
+            String htmlContent = templateEngine.process(emailTemplateName, context);
 
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(htmlContent, true);
 
+            byte[] pdfContent = pdfGenerationService.generatePdf(pdfTemplateName, context);
+            helper.addAttachment(pdfFileName, new ByteArrayResource(pdfContent));
+
             mailSender.send(message);
         } catch (MessagingException e) {
             throw new RuntimeException("Failed to send email", e);
+        } catch (DocumentException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
