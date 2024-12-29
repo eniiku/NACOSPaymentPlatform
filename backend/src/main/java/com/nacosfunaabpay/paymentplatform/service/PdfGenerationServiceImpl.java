7575cb3f65a55b13289067c3b8276d32acf30365
David Enikuomehin <enikuauth@gmail.com>
2024-12-29 14:51:01 +0100

    feat(backend): reference logo image in pdfs

--- backend/src/main/java/com/nacosfunaabpay/paymentplatform/service/PdfGenerationServiceImpl.java
+++ backend/src/main/java/com/nacosfunaabpay/paymentplatform/service/PdfGenerationServiceImpl.java
@@ -4,13 +4,19 @@ import com.lowagie.text.DocumentException;
 import org.aspectj.apache.bcel.util.ClassPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.util.ResourceUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.util.Base64;
 
 @Service
 public class PdfGenerationServiceImpl implements PdfGenerationService {
@@ -26,11 +32,15 @@ public class PdfGenerationServiceImpl implements PdfGenerationService {
     public byte[] generatePdf(String templateName, Context context) throws DocumentException, IOException {
         try {
             logger.info("Generating PDF from template: {}", templateName);
+
+            ClassPathResource resource = new ClassPathResource("static/images/logo.png");
+            String logoPath = resource.getFile().getAbsolutePath();
+            context.setVariable("logoSrc", "file:///" + logoPath);
+
             String htmlContent = templateEngine.process(String.valueOf(new ClassPath("pdf/" + templateName)), context);
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
             ITextRenderer renderer = new ITextRenderer();
-            renderer.getSharedContext().setDotsPerPixel(96);
             renderer.setDocumentFromString(htmlContent);
             renderer.layout();
             renderer.createPDF(outputStream);
